db1384a7dba9a2aa556635d8694343292a0bde5d
Pasha_Kovalev <pasha0kovalev@gmail.com>
2021-03-13 18:08:35 +0200

    add 7 task

--- Lesson 16/main.py
+++ Lesson 16/main.py
@@ -1,27 +1,60 @@
-class Employee:
-    # name="Ivan"
-    # zp_day=100
+import datetime
 
-    def __init__(self, name, zp_day, day):
+class Employee:
+    # name=" "
+    # zp_day=1
+    # days=1
+    # zp_mo=zp_day*days
+
+    def __init__(self, name, zp_day, days):
         self.name = name
         self.zp_day= zp_day
-        self.day= day
+        self.days= days
 
     def work(self):
         return "I come to the office"
 
-    def cheak_salary(self, zp_day, days):
-        return zp_day * days
+    def cheak_salary(self):
+        def zp(zp_day):
+            data_now=datetime.date.today().day
+            working_day=0
+            while data_now>0:
+                if data_now > 5:
+                    working_day+=5
+                else:
+                    working_day+=data_now
+                data_now-=7
+            return zp_day * working_day
+        zp_now=zp(self.zp_day)
+        zp_month= self.zp_day * self.days
+        return f"Зарплата за месяц: {zp_month}. Зарплата на текущий рабочий день: {zp_now}"
 
-    # def
+    def __gt__(self, other):
+        return self.cheak_salary() > other.cheak_salary()
+
+    def __ge__(self, other):
+        return self.cheak_salary() >= other.cheak_salary()
+
+    def __lt__(self, other):
+        return self.cheak_salary() < other.cheak_salary()
+
+    def __le__(self, other):
+        return self.cheak_salary() <= other.cheak_salary()
+
+    def __eq__(self, other):
+        return self.cheak_salary() == other.cheak_salary()
+
+    def __ne__(self, other):
+        return self.cheak_salary() != other.cheak_salary()
+
 
 class Recruiter(Employee):
+
     def work(self):
         return 'I come to the office and start to coding.'
 
     def __str__(self):
         return f"{self.__class__.__name__} : {self.name}"
-
 
 class Programmer(Employee):
     def work(self):
@@ -31,9 +64,16 @@ class Programmer(Employee):
         return f"{self.__class__.__name__} : {self.name}"
 
 
-a=Recruiter("Ivan", 100, 20)
+a=Recruiter("Ivan", 100, 23)
+print(a.work())
+print(a.cheak_salary())
 print(a)
 
+print()
 
-b=Programmer("Alex", 200, 20)
+b=Programmer("Alex", 200, 23)
+print(b.work())
+print(b.cheak_salary())
 print(b)
+
+print(a<b)
