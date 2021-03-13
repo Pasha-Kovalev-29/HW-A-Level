05fafba5c42e2cfb34a6a406ad6dbb606b8416b9
Pasha_Kovalev <pasha0kovalev@gmail.com>
2021-03-13 19:25:07 +0200

    add 8 task

--- Lesson 16/main.py
+++ Lesson 16/main.py
@@ -26,23 +26,26 @@ class Employee:
                 data_now-=7
             return zp_day * working_day
         zp_now=zp(self.zp_day)
-        zp_month= self.zp_day * self.days
+        zp_month=Employee.zp_month_calc(self)
         return f"Зарплата за месяц: {zp_month}. Зарплата на текущий рабочий день: {zp_now}"
 
+    def zp_month_calc(self):
+        return self.zp_day * self.days
+
     def __gt__(self, other):
-        return self.cheak_salary() > other.cheak_salary()
+        return self.zp_month_calc() > other.zp_month_calc()
 
     def __ge__(self, other):
-        return self.cheak_salary() >= other.cheak_salary()
+        return self.zp_month_calc() >= other.zp_month_calc()
 
     def __lt__(self, other):
-        return self.cheak_salary() < other.cheak_salary()
+        return self.zp_month_calc() < other.zp_month_calc()
 
     def __le__(self, other):
-        return self.cheak_salary() <= other.cheak_salary()
+        return self.zp_month_calc() <= other.zp_month_calc()
 
     def __eq__(self, other):
-        return self.cheak_salary() == other.cheak_salary()
+        return self.zp_month_calc() == other.zp_month_calc()
 
     def __ne__(self, other):
         return self.cheak_salary() != other.cheak_salary()
@@ -54,6 +57,8 @@ class Recruiter(Employee):
         return 'I come to the office and start to coding.'
 
     def __str__(self):
+        print(a.work())
+        print(a.cheak_salary())
         return f"{self.__class__.__name__} : {self.name}"
 
 class Programmer(Employee):
@@ -61,19 +66,17 @@ class Programmer(Employee):
         return 'I come to the office and start to hiring.'
 
     def __str__(self):
+        print(b.work())
+        print(b.cheak_salary())
         return f"{self.__class__.__name__} : {self.name}"
 
 
 a=Recruiter("Ivan", 100, 23)
-print(a.work())
-print(a.cheak_salary())
 print(a)
 
 print()
 
 b=Programmer("Alex", 200, 23)
-print(b.work())
-print(b.cheak_salary())
 print(b)
 
 print(a<b)
