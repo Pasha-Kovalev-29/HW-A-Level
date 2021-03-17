1b4c282ce6c3cd952c5121d6d0c3dd41a7078158
Pasha_Kovalev <pasha0kovalev@gmail.com>
2021-03-17 10:07:35 +0200

    fixed 8 task

--- Lesson 16/main.py
+++ Lesson 16/main.py
@@ -1,32 +1,30 @@
 import datetime
 
-class Employee:
-    # name=" "
-    # zp_day=1
-    # days=1
-    # zp_mo=zp_day*days
 
+class Employee:
     def __init__(self, name, zp_day, days):
         self.name = name
-        self.zp_day= zp_day
-        self.days= days
+        self.zp_day = zp_day
+        self.days = days
 
     def work(self):
         return "I come to the office"
 
     def cheak_salary(self):
         def zp(zp_day):
-            data_now=datetime.date.today().day
-            working_day=0
-            while data_now>0:
-                if data_now > 5:
-                    working_day+=5
-                else:
-                    working_day+=data_now
-                data_now-=7
-            return zp_day * working_day
-        zp_now=zp(self.zp_day)
-        zp_month=Employee.zp_month_calc(self)
+            first_data = datetime.date.today() - datetime.timedelta(days=datetime.date.today().day)
+            data_now = datetime.date.today()
+            delta = datetime.timedelta(1)
+
+            days = 0
+            while first_data != data_now:
+                if data_now.isoweekday() not in (6, 7):
+                    days += 1
+                data_now -= delta
+            return zp_day * days
+
+        zp_now = zp(self.zp_day)
+        zp_month = Employee.zp_month_calc(self)
         return f"Зарплата за месяц: {zp_month}. Зарплата на текущий рабочий день: {zp_now}"
 
     def zp_month_calc(self):
@@ -57,26 +55,28 @@ class Recruiter(Employee):
         return 'I come to the office and start to coding.'
 
     def __str__(self):
-        print(a.work())
-        print(a.cheak_salary())
         return f"{self.__class__.__name__} : {self.name}"
 
+
 class Programmer(Employee):
+
     def work(self):
         return 'I come to the office and start to hiring.'
 
     def __str__(self):
-        print(b.work())
-        print(b.cheak_salary())
         return f"{self.__class__.__name__} : {self.name}"
 
 
-a=Recruiter("Ivan", 100, 23)
+a = Recruiter("Ivan", 100, 23)
 print(a)
+print(a.work())
+print(a.cheak_salary())
 
 print()
 
-b=Programmer("Alex", 200, 23)
+b = Programmer("Alex", 200, 23)
 print(b)
+print(b.work())
+print(b.cheak_salary())
 
-print(a<b)
+c = Programmer("Alex2", 200, 23)
